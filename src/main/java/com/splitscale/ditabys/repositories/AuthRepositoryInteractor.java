e34c6ed7fade952ae0e7599c42fe27caf5a016f1
Steve <steven.ballaret-17@cpu.edu.ph>
2022-12-14 17:02:11 +0800

    Auth interactor implemented

---
+++ src/main/java/com/splitscale/ditabys/repositories/AuthRepositoryInteractor.java
@@ -0,0 +1,34 @@
+package com.splitscale.ditabys.repositories;
+
+import java.security.PublicKey;
+
+import com.splitscale.shield.auth.Token;
+import com.splitscale.shield.repository.AuthRepository;
+
+public class AuthRepositoryInteractor implements AuthRepository {
+
+  @Override
+  public boolean deleteByID(Long arg0) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public PublicKey getByID(String arg0) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean insert(Token arg0) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean update(Token arg0) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+  
+}
