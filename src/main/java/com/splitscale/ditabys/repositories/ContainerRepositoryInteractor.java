996071d0f82fd4d1287432c4117d27fc60d4c78e
Aljason Javier <aljasonjavier@gmail.com>
2022-12-21 21:32:04 +0800

    Implement container repo Fixes #3

---
+++ src/main/java/com/splitscale/ditabys/repositories/ContainerRepositoryInteractor.java
@@ -0,0 +1,56 @@
+package com.splitscale.ditabys.repositories;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.splitscale.fordastore.core.container.Container;
+import com.splitscale.fordastore.core.container.ContainerRequest;
+import com.splitscale.fordastore.core.repositories.ContainerRepository;
+
+public class ContainerRepositoryInteractor implements ContainerRepository {
+
+    @Override
+    public Container add(ContainerRequest arg0) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean delete(long arg0) throws IOException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Container getByContainerID(long arg0) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Container getByUid(String arg0) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Container> getListByName(String arg0) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Container> getListByUid(String arg0) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Container update(Container arg0) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    
+}
