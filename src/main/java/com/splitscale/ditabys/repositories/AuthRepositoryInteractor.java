5c755fc2ca5d0c1b1413069b29b8a53be0aa3c2e
kasutu <jeromecabugwason2@protonmail.com>
2022-12-29 17:19:41 +0800

    auth: Add interactor logic
    Description:
    added interactor logic for auth repo
    
    Expected behavior:
    able to search and delete in shield db

--- src/main/java/com/splitscale/ditabys/repositories/AuthRepositoryInteractor.java
+++ src/main/java/com/splitscale/ditabys/repositories/AuthRepositoryInteractor.java
@@ -3,6 +3,7 @@ package com.splitscale.ditabys.repositories;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.splitscale.ditabys.driver.AuthDbDriver;
@@ -18,13 +19,48 @@ public class AuthRepositoryInteractor implements AuthRepository {
   }
 
   @Override
-  public void deleteByUid(String uid) {
-    // implement me
+  public void deleteByUid(String uid) throws IOException {
+    String query = "DELETE FROM public_key WHERE key_id = UUID_TO_BIN(?);";
+
+    try {
+      Connection conn = db.getConnection();
+
+      PreparedStatement pstmt = conn.prepareStatement(query);
+      pstmt.setString(1, uid);
+
+      pstmt.executeUpdate();
+
+      conn.close();
+
+    } catch (SQLException e) {
+      throw new IOException("Could not delete token due to server error: " + e.getMessage());
+    }
   }
 
   @Override
-  public AuthPublicKey getByUid(String uid) {
-    return null;
+  public AuthPublicKey getByUid(String uid) throws IOException {
+    String query = "SELECT BIN_TO_UUID(key_id) as key_id, key_value FROM public_key WHERE key_id = UUID_TO_BIN(?);";
+
+    try {
+      Connection conn = db.getConnection();
+
+      PreparedStatement pstmt = conn.prepareStatement(query);
+      pstmt.setString(1, uid);
+
+      ResultSet rs = pstmt.executeQuery();
+
+      AuthPublicKey authPublicKey = new AuthPublicKey();
+
+      if (rs.next()) {
+        authPublicKey.setUid(rs.getString("key_id"));
+        authPublicKey.setPublicKey(rs.getString("key_value"));
+      }
+
+      conn.close();
+      return authPublicKey;
+    } catch (SQLException e) {
+      throw new IOException("Unable to find token by uid due to server error: " + e.getMessage());
+    }
   }
 
   @Override
@@ -43,13 +79,28 @@ public class AuthRepositoryInteractor implements AuthRepository {
       conn.close();
 
     } catch (SQLException e) {
-      throw new IOException("Could not add user to database due to server error: " + e.getMessage());
+      throw new IOException("Could not add token due to server error: " + e.getMessage());
     }
   }
 
   @Override
-  public void update(AuthPublicKey arg0) throws IOException {
-    // implement me
+  public void update(AuthPublicKey authPublicKey) throws IOException {
+    String query = "UPDATE public_key SET key_value = ? WHERE key_id = UUID_TO_BIN(?);";
+
+    try {
+      Connection conn = db.getConnection();
+
+      PreparedStatement pstmt = conn.prepareStatement(query);
+      pstmt.setString(1, authPublicKey.getPublicKey());
+      pstmt.setString(2, authPublicKey.getUid());
+
+      pstmt.executeUpdate();
+
+      conn.close();
+
+    } catch (SQLException e) {
+      throw new IOException("Could update token due to server error: " + e.getMessage());
+    }
   }
 
 }
