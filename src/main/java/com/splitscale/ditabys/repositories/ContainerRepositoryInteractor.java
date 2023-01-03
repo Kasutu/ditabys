686383c5a7605a5e892509ef1c185522f5804967
Jerome Cabugwason <jeromecabugwason2@protonmail.com>
2023-01-04 01:14:53 +0800

    Dev steve (#14)
    * Implemented usecases in container and url
    
    * refactor typo
    
    * container: Fix return type
    
    Description:
    fixed  ResponseEntity<List<ContainerResponse>> return type
    
    Expected behavior:
    should not get uid when returning a container list a single container
    
    Co-authored-by: Steve <steven.ballaret-17@cpu.edu.ph>

--- src/main/java/com/splitscale/ditabys/repositories/ContainerRepositoryInteractor.java
+++ src/main/java/com/splitscale/ditabys/repositories/ContainerRepositoryInteractor.java
@@ -12,7 +12,6 @@ import java.util.List;
 import com.splitscale.ditabys.driver.DatabaseDriver;
 import com.splitscale.ditabys.driver.StoreDbDriver;
 import com.splitscale.fordastore.core.container.Container;
-import com.splitscale.fordastore.core.container.ContainerRequest;
 import com.splitscale.fordastore.core.repositories.ContainerRepository;
 
 public class ContainerRepositoryInteractor implements ContainerRepository {
@@ -23,39 +22,38 @@ public class ContainerRepositoryInteractor implements ContainerRepository {
   }
 
   @Override
-  public void add(ContainerRequest containerRequest) throws IOException {
+  public Container add(String containerName, String uid) throws IOException {
     final String query = "INSERT INTO container (container_id, container_title, user_id) VALUES (null,?,UUID_TO_BIN(?))";
 
-    final String containerTitle = containerRequest.getName();
-    final String userId = containerRequest.getUid();
-
     Container container = new Container();
-    container.setName(containerTitle);
-    container.setUid(userId);
+    container.setUid(uid);
+    container.setName(containerName);
 
     try {
       Connection conn = db.getConnection();
       PreparedStatement pstmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-      pstmt.setString(1, containerTitle);
-      pstmt.setString(2, userId);
+      pstmt.setString(1, containerName);
+      pstmt.setString(2, uid);
 
-      int rowAffected = pstmt.executeUpdate();
-      if (rowAffected == 1) {
-        ResultSet rs = pstmt.getGeneratedKeys();
-        if (rs.next()) {
-          long id = rs.getLong(1);
-          container.setContainerID(id);
-        }
+      pstmt.executeUpdate();
+
+      ResultSet rs = pstmt.getGeneratedKeys();
+
+      if (rs.next()) {
+        long id = rs.getLong(1);
+
+        container.setContainerID(id);
       }
 
       conn.close();
+      return container;
     } catch (SQLException e) {
       throw new IOException("Could not add a new container to database" + e.getMessage());
     }
   }
 
   @Override
-  public void delete(long containerId) throws IOException {
+  public void delete(Long containerId) throws IOException {
     String query = "DELETE FROM container WHERE container_id = ?";
 
     try {
@@ -74,14 +72,14 @@ public class ContainerRepositoryInteractor implements ContainerRepository {
   }
 
   @Override
-  public Container getByContainerID(long container_id) throws IOException {
+  public Container getByContainerID(long containerId) throws IOException {
     final String query = "Select * From container WHERE container_id = ?";
     Container container = new Container();
 
     try {
       Connection conn = db.getConnection();
       PreparedStatement pstmt = conn.prepareStatement(query);
-      pstmt.setLong(1, container_id);
+      pstmt.setLong(1, containerId);
       ResultSet rs = pstmt.executeQuery();
 
       if (rs.next()) {
@@ -96,19 +94,20 @@ public class ContainerRepositoryInteractor implements ContainerRepository {
   }
 
   @Override
-  public Container getByUid(String user_id) throws IOException {
+  public Container getByUid(String userId) throws IOException {
     final String query = "Select * From user WHERE user_id = UUID_TO_BIN(?)";
     Container container = new Container();
 
     try {
       Connection conn = db.getConnection();
       PreparedStatement pstmt = conn.prepareStatement(query);
-      pstmt.setString(1, user_id);
+      pstmt.setString(1, userId);
       ResultSet rs = pstmt.executeQuery();
 
       if (rs.next()) {
         container.setUid(rs.getString("user_id"));
       }
+
       conn.close();
       return container;
 
@@ -118,13 +117,13 @@ public class ContainerRepositoryInteractor implements ContainerRepository {
   }
 
   @Override
-  public List<Container> getListByName(String container_title) throws IOException {
+  public List<Container> getListByName(String containerTitle) throws IOException {
     final String query = "SELECT * FROM container WHERE container_title LIKE ?";
 
     try {
       Connection conn = db.getConnection();
       PreparedStatement pstmt = conn.prepareStatement(query);
-      pstmt.setString(1, "%" + container_title + "%");
+      pstmt.setString(1, "%" + containerTitle + "%");
       ResultSet rs = pstmt.executeQuery();
       List<Container> containers = new ArrayList<>();
 
@@ -141,42 +140,50 @@ public class ContainerRepositoryInteractor implements ContainerRepository {
   }
 
   @Override
-  public List<Container> getListByUid(String container_id) throws IOException {
-    final String query = "SELECT * FROM container WHERE container_id = ?";
+  public List<Container> getListByUid(String uid) throws IOException {
+    final String query = "SELECT container_id, BIN_TO_UUID(user_id) as user_id, container_title FROM container WHERE user_id = UUID_TO_BIN(?)";
 
     try {
       Connection conn = db.getConnection();
       PreparedStatement pstmt = conn.prepareStatement(query);
-      pstmt.setString(1, container_id);
+      pstmt.setString(1, uid);
+
       ResultSet rs = pstmt.executeQuery();
+
       List<Container> containers = new ArrayList<>();
 
       while (rs.next()) {
         Container container = new Container();
-        container.setName(rs.getString("container_id"));
+
+        container.setContainerID(rs.getLong("container_id"));
+        container.setName(rs.getString("container_title"));
+        container.setUid(rs.getString("user_id"));
+
         containers.add(container);
       }
+
       conn.close();
       return containers;
-
     } catch (SQLException e) {
-      throw new IOException("Could not get a list of this UID");
+      throw new IOException("Could not get a list of this UID: " + e.getMessage());
     }
   }
 
   @Override
-  public void update(Container container) throws IOException {
+  public void update(String containerName, Long containerId) throws IOException {
     final String query = "UPDATE container SET container_title = ? WHERE container_id = ?";
 
     try {
       Connection conn = db.getConnection();
+
       PreparedStatement pstmt = conn.prepareStatement(query);
-      pstmt.setString(1, container.getName());
-      pstmt.setLong(2, container.getContainerID());
+      pstmt.setString(1, containerName);
+      pstmt.setLong(2, containerId);
       pstmt.executeUpdate();
+
+      conn.close();
     } catch (SQLException e) {
       throw new IOException("Unable to update Container");
     }
   }
-
 }
